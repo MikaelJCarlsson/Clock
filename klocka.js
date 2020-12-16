4130d5c4f971a29449829530238d455823458f46
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2020-12-16 16:50:40 +0100

    birf2

--- klocka.js
+++ klocka.js
@@ -1,30 +1,9 @@
+$(function(){
 
-function showTime(){
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var d = date.getDay();
-
-    var weekday=new Array(7);
-    weekday[0]="Sunday";
-    weekday[1]="Monday";
-    weekday[2]="Tuesday";
-    weekday[3]="Wednesday";
-    weekday[4]="Thursday";
-    weekday[5]="Friday";
-    weekday[6]="Saturday";
-    
-    h = (h < 10) ? "0" + h : h; //Ternary operator to check if time is less than 10, append 0.
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + weekday[d];
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
-
-showTime();
+	if (!$('.envelope').hasClass('open')){
+	  $('.envelope').click(function(){
+		$(this).removeClass('new').addClass('open');
+	  });
+	}
+	
+  });
